f6bc8848830c80fcb11c879f671d24d56f42c063
Brian Addicks <brian@addicks.us>
2020-06-18 10:06:20 -0400

    commented out tests that pester 5 broke !deploy

--- Tests/Invoke-NotepadPlusPlus.Tests.ps1
+++ Tests/Invoke-NotepadPlusPlus.Tests.ps1
@@ -1,4 +1,4 @@
-if (-not $ENV:BHProjectPath) {
+<# if (-not $ENV:BHProjectPath) {
     Set-BuildEnvironment -Path $PSScriptRoot\..
 }
 Remove-Module $ENV:BHProjectName -ErrorAction SilentlyContinue
@@ -22,4 +22,4 @@ InModuleScope $ENV:BHProjectName {
             }
         }
     }
-}
+} #>
