05959ab30dc3795f9d3df9be307ba0c163c35883
Brian Addicks <brian@addicks.us>
2020-06-19 13:17:25 -0400

    added Copy-PsObjectWithNewProperty

---
+++ CorkScrew/Public/Copy-PsObjectWithNewProperty.ps1
@@ -0,0 +1,29 @@
+function Copy-PsObjectWithNewProperty {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $True)]
+        $PsObject,
+
+        [Parameter(Mandatory = $True, Position = 1)]
+        [string[]]$NewProperty
+    )
+
+    BEGIN {
+        $VerbosePrefix = "Copy-PsObjectWithNewProperty:"
+    }
+
+    PROCESS {
+        $Properties = ($PsObject | Get-Member -MemberType Property).Name
+        $AllProperties = $Properties + $NewProperty
+
+        $ReturnObject = "" | Select-Object $AllProperties
+
+        foreach ($property in $Properties) {
+            $ReturnObject.$property = $PsObject.$property
+        }
+    }
+
+    END {
+        $ReturnObject
+    }
+}
